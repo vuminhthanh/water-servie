be8b6c6bc2a3cad5b0ab24a02007c6748c8262d5
thanhvm <thanhvm@9pay.vn>
2026-08-25 15:46:41 +0700

    docker

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Filament\Filament;
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\URL;
 class AppServiceProvider extends ServiceProvider
 {
     /**
