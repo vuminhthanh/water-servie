82ef4c888742d073c1e2490e7722079f0775629f
thanhvm <thanhvm@9pay.vn>
2026-08-25 16:22:13 +0700

    docker

--- server.php
+++ server.php
@@ -14,8 +14,33 @@ $uri = urldecode(
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
-if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
-    return false;
+$publicPath = realpath(__DIR__.'/public');
+$file = realpath(__DIR__.'/public'.$uri);
+
+if ($uri !== '/' && $file && strpos($file, $publicPath.DIRECTORY_SEPARATOR) === 0 && is_file($file)) {
+    $extension = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+    $contentTypes = [
+        'css' => 'text/css; charset=UTF-8',
+        'js' => 'application/javascript; charset=UTF-8',
+        'json' => 'application/json; charset=UTF-8',
+        'svg' => 'image/svg+xml',
+        'png' => 'image/png',
+        'jpg' => 'image/jpeg',
+        'jpeg' => 'image/jpeg',
+        'gif' => 'image/gif',
+        'webp' => 'image/webp',
+        'ico' => 'image/x-icon',
+        'woff' => 'font/woff',
+        'woff2' => 'font/woff2',
+    ];
+
+    if (isset($contentTypes[$extension])) {
+        header('Content-Type: '.$contentTypes[$extension]);
+    }
+
+    header('Content-Length: '.filesize($file));
+    readfile($file);
+    return true;
 }
 
 require_once __DIR__.'/public/index.php';
